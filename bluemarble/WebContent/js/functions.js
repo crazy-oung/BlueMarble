3908b17aa5f55eef703ad946ef649d0c0b8996b5
crazy-oung <d_yng@naver.com>
2019-11-13 18:27:25 +0900

    말 움직임 함수 와  상호 작용 하는 함수 정리 및 조건문 정리 우주여행 함수 수정중
    사회 복지 기금 땅 도착시 기부금 받기 및 내기 기능 구현

--- bluemarble/WebContent/js/functions.js
+++ bluemarble/WebContent/js/functions.js
@@ -28,11 +28,10 @@ function gameStart(players) {
 	}
 	
 	//초기화
-	state = turn = 0;
-	point = [[1, 1, 1, 1], [1,1,1,1]];
-	turnCount = round = [0, 0, 0, 0];
-	
-	console.log(state, turn, point, turnCount);
+	state = turn = donation = 0;	// 차례, 진행된 턴, 기부금
+	point = [[1, 1, 1, 1], [1,1,1,1]];	// 방문 땅
+	turnCount = round = [0, 0, 0, 0];	// 무인도 잔류 턴수, 돌은 바퀴수 
+	console.log("초기화: "+state, turn, donation, point, turnCount, round);
 	
 	// n인 스타트 ! 알림
 	$("#playersTurn").empty();
@@ -40,7 +39,7 @@ function gameStart(players) {
 	$("#playersTurn").append("<strong class='text-primary'>플레이어"+(state+1)+"</strong>님 차례입니다!");	
 }
 
-
+// 주사위 굴림 함수 
 function rollDice() {
 	console.log("rolled Dice!!");
 	
@@ -69,17 +68,21 @@ function atDesertIsland(ran1, ran2){
 		console.log("더블이 떴는데 무인도 도착.. ㅜ");
 		return true;
 	}
-	if(ran1 != ran2 && turnCount[state] <4){
-		// 더블이 아니고 턴수가 충족되지 않으면  여전히 무인도에 
+	
+	// 더블이 아니고 턴수가 충족되지 않으면  무인도에 잔류
+	if(ran1 != ran2 && turnCount[state] <4){ 
 		console.log("더블도 아니고 3턴도 지나지 않았군요. 한턴 쉽니다. 무인도에 머문 턴수: "+turnCount[state]);
 		return true;
 	}
+	
+	// 더블이 나오고 1턴 이상 잔류 했을 시 탈출.
 	if(ran1==ran2 && turnCount[state]>=2){
 		turnCount[state] = 0;
 		console.log("더블이다!!! 무인도 탈출 !");
 		return false;
 	}
 	
+	// 4턴이상 잔류했을 때 탈출한다.
 	turnCount[state] = 0;
 	console.log("무인도 탈출~!");		
 	return false;
@@ -97,21 +100,18 @@ function moveTo() {
 	return;
 }
 
-
+// 월급 지급 함수
 function getSalary() {
 	console.log("getSalary 월급을 받습니다.")
 	
 	$("#playersTurn").append("<p class='text-success italic bold' style='margin-top:8px; font-style:italic'>플레이어"+(state+1)+"님에게 월급지급!</p>");
 	money[state] +=20;	// 월급 지급 
 	console.log("잔고: "+money[state]);
-	$("#"+(state+1)+"pMoney").text(money[state])
+	resetPlayerMoney(state);
 	return;
 }
 
-// 더블 검사 함수 
-function checkDouble(){
-	
-}
+ 
 
 // 주사위 굴려 게임 진행하기
 function letsMove(ran1, ran2) {	
@@ -148,70 +148,80 @@ function letsMove(ran1, ran2) {
 	
 	
 	// 말 이동 -> 1. 말 제거 2. 도착 한 칸에 말 출력
-	$("#p"+point[0][state]).children("b").remove("#"+(state+1)+"p");
-	
-	// 말 이동
+	$("#p"+point[0][state]).children("b").remove("#"+(state+1)+"p"); 
 	let afterId = "#p" + point[1][state];
 	$(afterId).append(player[state]);
 	point[0][state] = point[1][state];
 	
-	// 말을 움직이고 무인도 확인.
-	if(point[1][state] == 11){
-		setTimeout(function() {	// 말이 무인도로 이동한 모습을 보여주고 알림
-			alert("무인도 입니다! 갇히셨어요! :( ");
-			atDesertIsland(ran1, ran2);
-		}, 200)	
-		state++;
-		checkMax();
-		return;
-	}
-	// 우주여행 칸에 도착하면 이번턴 쉬고 다음턴에 칸 선택해서 감 
-	if(point[1][state] == 31){
-		alert("우주여행 도착!! 다음 턴에 원하는 곳으로 이동 할 수 있어요 !");
-		return;
-		
-	}
+	// 도착한 곳?
+	let arrived = lands[0][point[1][state]-1];
+	console.log("도착한 곳 : ",arrived,"플레이어: ", state+1);
 	
-	// 무인도 더블을 확인하기 위함.
-	if(ran1 == ran2){
-		console.log("더블이다! 한번더!");
-		if(point[1][state] == 11 || point[0][state] == 11){
-			state++;
-			console.log("취소")
-			return;
-		}		
-		$("#playersTurn").append("<p class='text-warning italic' style='margin-top:8px; font-style:italic'>더블입니다! 한번 더!</p>");		
-	}
-	
-	console.log("도착한 곳 : ",lands[0][point[1][state]-1]);
+	// 플레이어가 도착한 지점 검사 
 	switch (point[1][state]) {
+	
 		// 출발지점
 		case 1:
-			alert("출발 지점이에요 ! 월급을 받았어요 :) \n 현재 나의 잔고에는 "+money[state]+"만원이 있어요");
-			break;
+			alert("출발 지점이에요 ! 월급을 받았어요 :) \n 현재 나의 잔고에는 "+money[state]+"만원이 있어요");	// 주사위 굴리면서 월급을 지급했음. 여기서는 월급 지급할 필요 없음.
+			checkDouble(ran1, ran2);
+			return; 
+			
+		// 무인도 
+		case 11:
+			 // 도착한 곳이 무인도 이면 리턴.
+			setTimeout(function() {	// 말이 무인도로 이동한 모습을 보여주고 알림
+				alert("무인도 입니다! 갇히셨어요! :( ");
+				atDesertIsland(ran1, ran2);
+			}, 150)	 
+			checkMax();
+			return; 
+			
 		// 접수처
 		case 21:
 			alert("복지기금 접수처 입니다. 모든 기부금을 기부 받습니다 !");
-			break;
+			console.log("받을금액: ",donation,", 현재 잔액: ",money[state]);
+			money[state] += donation;
+			donation = 0;
+			console.log("기부금액: ",donation,", 현재 잔액: ",money[state]);
+			resetPlayerMoney(state);// 머니 정보 갱신 
+			checkDouble(ran1, ran2);
+			return;
+			
 		// 기부
 		case 39:
-			alert("기부 해야 합니다. ! 20만원을 사회에 기부하세요. \n 좋은일을 하시는 군요? :D");
+			alert("기부 해야 합니다. ! 20만원을 사회에 기부하세요. \n 좋은일을 하시는군요? :D");
+			console.log("잔액: "+money[state]);
+			donation+=20;
+			money[state] -= 20;
+			console.log("누적 기부금액 : "+donation+"지불 후 잔액: "+money[state]);
+			resetPlayerMoney(state)
+			checkDouble(ran1, ran2);
 			break;
+		
+		case 31:
+			alert("우주여행 도착!! 다음 턴에 원하는 곳으로 이동 할 수 있어요 !");
+			return; 
+			
 		default:
-			if(lands[0][point[1][state]-1] === "🔑 황금열쇠"){
-				console.log("황금 열쇠를 뽑습니다.!")
+			if(arrived === "🔑 황금열쇠"){
+				alert("🔑황금 열쇠를 뽑습니다.!")
+				let gkcard = Math.floor(Math.random() * goldenKey.length);// 0~ 황금열쇠크기
+				alert(goldenKey[gkcard]);
+				
+				checkDouble(ran1, ran2); 
+				return;
 			}
 			break;
 	}
 
-	if(ran1 == ran2){
-		state--;
-	}
+	
 	
 	// 토지 정보 알림창에 입력
-	insertLandInfo(point[1][state]-1, state);
+	insertLandInfo(point[1][state]-1, state); 
 	// 알림창 출력
 	 $("#landConfirm").modal({backdrop: 'static'});
+	 
+	 checkDouble(ran1, ran2);
 	
 }
 
@@ -250,6 +260,7 @@ function insertLandInfo(landNum, state) {
 	
 }
 
+// Max state 검사
 function checkMax() {
 	console.log("checkMax")
 	if(state > maxState){
@@ -259,5 +270,22 @@ function checkMax() {
 		console.log("턴증가! ",turn,"state = "+state);
 	}
 }
+
+// 특정 플레이어 돈 정보 갱신
+function resetPlayerMoney(state){
+	console.log((state+1)+"플레이어 머니정보 갱신");
+	$("#"+(state+1)+"pMoney").text(money[state]);
+}
+
+// 더블 검사 
+function checkDouble(ran1, ran2){
+	if(ran1 == ran2){
+		state--;
+		$("#playersTurn").append("<p class='text-warning italic' style='margin-top:8px; font-style:italic'>더블입니다! 한번 더!</p>");
+	}
+}
+
+
+func
 
 
